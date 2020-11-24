6e056097a4c86be87451dcd517a47808a442205c
mmporong <74720422+mmporong@users.noreply.github.com>
2020-11-25 00:11:48 +0900

    Add files via upload

--- Ripple/ripple.js
+++ Ripple/ripple.js
@@ -28,7 +28,7 @@ export class Ripple {
         }
 
         ctx.beginPath();
-        ctx.fillStyle = '#00ff00';
+        ctx.fillStyle = '#000';
         ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
         ctx.fill();
     }
